10b0de9856d74f3d35942b2e1a6e0f74cf62dd5a
hyeg <hyeg0121@naver.com>
2023-09-04 19:41:18 +0900

    시간 지난 뒤 선택지 보이게 하기, 배경 이미지 추가하기, 쿼리 스트링으로 id값 가져오기

--- js/data1.js
+++ js/data1.js
@@ -5,36 +5,56 @@
 let data = {
     "question" : [
         {
-            "id": 1,
-            "content" : "질문내용입니다.1"
+            "id": 0,
+            "content" : "아침에 알림이 울렸다. 일어나겠습까"
         },
         {
-            "id": 2,
+            "id": 1,
             "content" : "질문내용입니다.2"
         },
         {
-            "id": 3,
+            "id": 2,
             "content" : "질문내용입니다.3"
         },
+        {
+            "id": 3,
+            "content" : "44444"
+        },
+        {
+            "id": 4,
+            "content" : "444"
+        }
     ],
     "choice": [
         {
-            "id": 1,
+            "id": 0,
             "content1" : "1질문의 선택지1",
             "content2" : "1질문의 선택지2",
             "content3" : "1질문의 선택지3"
         },
         {
-            "id": 2,
+            "id": 1,
             "content1" : "2질문의 선택지1",
             "content2" : "2질문의 선택지2",
             "content3" : "2질문의 선택지3"
         },
         {
-            "id": 3,
+            "id": 2,
             "content1" : "3질문의 선택지1",
             "content2" : "3질문의 선택지2",
             "content3" : "3질문의 선택지3"
+        },
+        {
+            "id": 3,
+            "content1" : "444",
+            "content2" : "444",
+            "content3" : "444"
+        },
+        {
+            "id": 4,
+            "content1" : "555",
+            "content2" : "555",
+            "content3" : "555"
         }
     ]
 };
